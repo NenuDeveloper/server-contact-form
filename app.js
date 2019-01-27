e1edb5db19e82d8588d801a74360b78a42e6ddba
Ángel Ordóñez Sánchez <nenust1@outlook.com>
2019-01-27 14:07:13 -0600

    deploy2

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -13,7 +11,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+var reCAPTCHA = require('recaptcha2');
+
+var recaptcha = new reCAPTCHA({
+  siteKey: '6LeQEI0UAAAAAAKtYruZr_M4ymtGLUtuFOqGw6YP',
+  secretKey: '6LeQEI0UAAAAAAlDXSfb7iPN5plL9wHYF3n9C4Z8'
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+    recaptcha.validateRequest(req)
+    .then(function(){
+      var name = req.body.name;
+      var email = req.body.email;
+      var phone = req.body.phone;
+      var message = req.body.message;
+
+
+      res.send(`El nombre es: ${name} El correo electrónico es: ${email} - El teléfono es: ${phone} - El mensaje es: ${message}`);
+    })
+    .catch((errorCodes) => {
+      res.json({
+        formsubmit: false,
+        errors: recaptcha.translateErrors(errorCodes)
+      });
+    })
+})
 
 app.get('*', (req, res) => {
     res.type('text').send('Not Found');
